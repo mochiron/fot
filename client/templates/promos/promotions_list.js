d04ee7dbfbcd3059d45c8f781f550a85e290c12f
mochiron <sshirasaka@gmail.com>
2015-02-25 18:02:29 +0000

    Initial commit

---
+++ client/templates/promos/promotions_list.js
@@ -0,0 +1,47 @@
+var promotionsData = [
+  {
+    title: 'Around the World in 80 days',
+    description: 'Single origin irish half and half id lungo and milk foam. White, whipped grounds aromatic beans turkish macchiato.',
+    imageUrl: 'img/airship2.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  },
+  {
+    title: 'London to Suez',
+    description: 'Grounds frappuccino cup variety skinny, medium foam sugar java medium skinny decaffeinated. Robusta blue mountain foam ut qui, arabica kopi-luwak, rich white redeye coffee body.',
+    imageUrl: 'img/airship3.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  },
+  {
+    title: 'Mumbai',
+    description: 'Milk, shop est siphon foam instant dripper. Filter irish body extraction shop java id, shop et in kopi-luwak sweet.',
+    imageUrl: 'img/airship4.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  },
+  {
+    title: 'Calcutta to Hong Kong',
+    description: 'Espresso dark whipped in, medium dripper saucer mug that pumpkin spice milk frappuccino. Whipped est so, white steamed sugar foam aroma latte.',
+    imageUrl: 'img/airship5.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  },
+  {
+    title: 'Around the World in 80 days',
+    description: 'That to go, that to go half and half, acerbic breve robust coffee beans body. Dripper, aged french press, café au lait aftertaste chicory variety mocha viennese caffeine beans galão.',
+    imageUrl: 'img/airship6.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  },
+  {
+    title: 'Around the World in 80 days',
+    description: 'Doppio single origin, café au lait, latte single shot a macchiato aroma. Kopi-luwak doppio extraction, coffee et trifecta, crema sweet galão rich americano.',
+    imageUrl: 'img/airship7.jpg',
+    linkTitle: 'Click here',
+    linkUrl:'#'
+  }
+];
+Template.promotionsList.helpers({
+  promotions: promotionsData
+});
